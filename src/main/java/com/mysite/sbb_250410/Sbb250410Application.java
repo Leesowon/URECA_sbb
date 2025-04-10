15afdbf86c0127254fdbe7ca1c63811e4f3b9006
Leesowon <leesowon00@naver.com>
2025-04-10 16:46:08 +0900

    first commit

---
+++ src/main/java/com/mysite/sbb_250410/Sbb250410Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_250410;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbb250410Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbb250410Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sbb_250410/Sbb250410ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sbb_250410;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb250410ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
